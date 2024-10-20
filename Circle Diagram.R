70c8709a38062503c79386df9b1dfde978177035
Aghbuul <116954361+Aghbuul@users.noreply.github.com>
2024-10-20 18:09:30 +0700

    Rotated words

--- Circle Diagram.R
+++ Circle Diagram.R
@@ -47,13 +47,14 @@ plot <- ggplot() +
   geom_segment(aes(x = O[1], y = O[2], xend = G[1], yend = G[2]), colour = "red", linewidth = 1.2) +
   geom_text(aes(x = A[1]+0.11, y = (O[2] + A[2]) / 2, label = "sin"), colour = "red", size = 7) +
   
-  # Blue cos line (O to C)
+  # Blue cos line
   geom_segment(aes(x = O[1], y = O[2], xend = C[1], yend = C[2]), colour = "blue", linewidth = 1.2) +
+  geom_segment(aes(x = A[1], y = A[2], xend = G[1], yend = G[2]), colour = "blue", linewidth = 1.2) +
   geom_text(aes(x = (O[1] + C[1]) / 2, y = O[2] - 0.05, label = "cos"), colour = "blue", size = 7) +
   
-  # Green versin line (C to D)
-  geom_segment(aes(x = C[1], y = C[2], xend = D[1], yend = D[2]), colour = "green", linewidth = 1.2) +
-  geom_text(aes(x = (C[1] + D[1]) / 2, y = C[2] - 0.05, label = "versin"), colour = "green", size = 7) +
+  # Spring green versin line (C to D)
+  geom_segment(aes(x = C[1], y = C[2], xend = D[1], yend = D[2]), colour = "springgreen3", linewidth = 1.2) +
+  geom_text(aes(x = (C[1] + D[1]) / 2, y = C[2] - 0.05, label = "versin"), colour = "springgreen3", size = 7) +
   
   # Cyan coversin line (G to H)
   geom_segment(aes(x = G[1], y = G[2], xend = H[1], yend = H[2]), colour = "cyan", linewidth = 1.2) +
@@ -64,7 +65,7 @@ plot <- ggplot() +
   geom_text(aes(x = -0.2, y = (H[2] + F[2]) / 2, label = "excsc"), colour = "darkgreen", size = 6) +
   
   # OL line
-  geom_segment(aes(x = O[1], y = O[2], xend = M[1], yend = M[2]), colour = "turquoise", linetype = "dotted", linewidth = 1.2) +
+  geom_segment(aes(x = O[1], y = O[2], xend = M[1], yend = M[2]), colour = "lightseagreen", linetype = "dotted", linewidth = 1.2) +
   geom_segment(aes(x = M[1], y = M[2], xend = L[1], yend = L[2]), colour = "gray", linetype = "longdash", linewidth = 1.2) +
   
   # OK line
@@ -76,11 +77,12 @@ plot <- ggplot() +
   
   # Orange cotangent line (F to A)
   geom_segment(aes(x = F[1], y = F[2], xend = A[1], yend = A[2]), colour = "orange", linewidth = 1.2) +
-  geom_text(aes(x = (F[1] + A[1]) / 2, y = F[2], label = "cot"), colour = "orange", size = 7) +
+  geom_segment(aes(x = F[1], y = F[2], xend = F[1]-0.55, yend = F[2]), colour = "orange", linetype = "dotted", linewidth = 1.2) +
+  geom_text(aes(x = (F[1] + A[1]) / 2, y = F[2] - 0.05, label = "cot"), colour = "orange", size = 7, angle = -33) +
   
   # Tan tangent line (E to A)
   geom_segment(aes(x = E[1], y = E[2], xend = A[1], yend = A[2]), colour = "tan", linewidth = 1.2) +
-  geom_text(aes(x = (E[1] + A[1]) / 2, y = (E[2] + A[2]) / 2, label = "tan"), colour = "tan", size = 7) +
+  geom_text(aes(x = (E[1] + A[1]) / 2, y = (E[2] + A[2]) / 2 + 0.15, label = "tan"), colour = "tan", size = 7, angle = -33) +
   
   # Light pink csc line with arrows
   geom_segment(aes(x = -0.5, y = F[2], xend = -0.5, yend = 0), colour = "lightpink", linewidth = 1.2,
@@ -88,29 +90,26 @@ plot <- ggplot() +
   geom_text(aes(x = -0.35, y = F[2] / 2, label = "csc"), colour = "lightpink", size = 7) +
   
   # Purple covercos line
-  geom_segment(aes(x = -0.65, y = A[2], xend = -0.65, yend = B[2]), colour = "purple", linewidth = 1.2,
+  geom_segment(aes(x = -0.65, y = A[2], xend = -0.65, yend = -1), colour = "purple", linewidth = 1.2,
                arrow = arrow(type = "closed", ends = "both", length = unit(0.2, "inches"))) +
-  geom_text(aes(x = -0.7, y = (A[2] + B[2]) / 2, label = "covercos"), colour = "purple", size = 7) +
+  geom_text(aes(x = -0.75, y = 0.15, label = "covercos"), colour = "purple", size = 7, angle = 90) +
   
-  # Blue vercos line
+  # Steel blue vercos line
   geom_segment(aes(x = K[1], y = K[2] - 0.25, xend = C[1], yend = C[2] - 0.25), colour = "steelblue", linewidth = 1.2,
                arrow = arrow(type = "closed", ends = "both", length = unit(0.2, "inches"))) +
   geom_text(aes(x = (K[1] + C[1]) / 2, y = K[2] - 0.3, label = "vercos"), colour = "steelblue", size = 7) +
   
-  # Turquoise sec line
-  geom_segment(aes(x = M[1], y = M[2] + 0.05, xend = E[1], yend = M[2] + 0.05), colour = "turquoise", linewidth = 1.2,
+  # Light sea green sec line
+  geom_segment(aes(x = M[1], y = M[2] + 0.05, xend = E[1], yend = M[2] + 0.05), colour = "lightseagreen", linewidth = 1.2,
                arrow = arrow(type = "closed", ends = "both", length = unit(0.2, "inches"))) +
-  geom_text(aes(x = (M[1] + E[1]) / 2, y = M[2] - 0.07, label = "sec"), colour = "turquoise", size = 7) +
+  geom_text(aes(x = (M[1] + E[1]) / 2, y = M[2] - 0.07, label = "sec"), colour = "lightseagreen", size = 7) +
 
   # Turquoise secant label line on the right
-  geom_segment(aes(x = E[1], y = E[2], xend = E[1], yend = M[2]-0.05), colour = "turquoise", linetype = "dotted", linewidth = 1.2) +
+  geom_segment(aes(x = E[1], y = E[2], xend = E[1], yend = M[2]-0.02), colour = "lightseagreen", linetype = "dotted", linewidth = 1.2) +
   
   # Light gray crd line (A to D)
   geom_segment(aes(x = A[1], y = A[2], xend = D[1], yend = D[2]), colour = "lightgray", linewidth = 1.2) +
-  geom_text(aes(x = (A[1] + D[1]) / 2, y = (A[2] + D[2]) / 2, label = "crd"), colour = "lightgray", size = 7) +
-  
-  # Sky blue A to H line
-  geom_segment(aes(x = A[1], y = A[2], xend = H[1], yend = H[2]), colour = "deepskyblue", linewidth = 1.2) +
+  geom_text(aes(x = (A[1] + D[1]) / 2 + 0.05, y = (A[2] + D[2]) / 2 + 0.05, label = "crd"), colour = "lightgray", size = 7, angle = -57) +
   
   # Pink exsec line
   geom_segment(aes(x = D[1], y = D[2], xend = E[1], yend = E[2]), colour = "pink", linewidth = 1.2) +
@@ -119,6 +118,7 @@ plot <- ggplot() +
   # Light gray dotted extra line 
   geom_segment(aes(x = K[1], y = K[2], xend = K[1], yend = K[2] - 0.5), colour = "lightgray", linetype = "dotted", linewidth = 1.2) +
   geom_segment(aes(x = L[1], y = L[2], xend = L[1]- 0.8, yend = L[2]), colour = "lightgray", linetype = "dotted", linewidth = 1.2) +
+  geom_segment(aes(x = G[1], y = G[2], xend = G[1]- 0.8, yend = G[2]), colour = "lightgray", linetype = "dotted", linewidth = 1.2) +
   
   
   # Add labels to each coordinate
@@ -130,11 +130,12 @@ plot <- ggplot() +
   geom_text(aes(x = E[1] + 0.07, y = E[2] - 0.07, label = "E"), colour = "black", size = 7) +
   geom_text(aes(x = F[1] + 0.00, y = F[2] + 0.08, label = "F"), colour = "black", size = 7) +
   geom_text(aes(x = G[1] - 0.07, y = G[2] - 0.07, label = "G"), colour = "black", size = 7) +
-  geom_text(aes(x = H[1] + 0.07, y = H[2] + 0.07, label = "H"), colour = "black", size = 7) +
+  geom_text(aes(x = H[1] + 0.07, y = H[2] + 0.02, label = "H"), colour = "black", size = 7) +
   geom_text(aes(x = K[1] - 0.10, y = K[2] + 0.00, label = "K"), colour = "black", size = 7) +
   geom_text(aes(x = L[1] + 0.00, y = L[2] - 0.07, label = "L"), colour = "black", size = 7) +
-  geom_text(aes(x = M[1] + 0.07, y = M[2] + 0.07, label = "M"), colour = "black", size = 7) +
-  geom_text(aes(x = N[1] + 0.07, y = N[2] + 0.07, label = "N"), colour = "black", size = 7) +
+  #I put M and N there to make it easier to set where the lines should go
+  #geom_text(aes(x = M[1] + 0.07, y = M[2] + 0.07, label = "M"), colour = "black", size = 7) +
+  #geom_text(aes(x = N[1] + 0.07, y = N[2] + 0.07, label = "N"), colour = "black", size = 7) +
   
   
   coord_fixed() +
